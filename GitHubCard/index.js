3051b110e4dd7bf0cc001f52b43e4ab7d9fdab4a
Elijah <elijahoneal@yahoo.com>
2020-11-05 21:59:15 -0500

    gitCardMaker constructor

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,9 +1,13 @@
+import axios from 'axios';
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
-
+axios.get('https://api.github.com/users/elijahoneal')
+.then(res =>console.log(res))
+.catch(err => err)
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -11,6 +15,60 @@
 
     Skip to STEP 3.
 */
+const gitCardMaker = data => {
+  // div class card
+  let card = document.createElement('div');
+  card.classList.add('card');
+
+// user img
+  let img = document.createElement('img');
+  img.setAttribute('src' , data.avatar_url );
+
+  // div class card-info
+  let cardInfo = document.createElement('div');
+  cardInfo.classList.add('card-info');
+  // h3 name
+  let h3 = document.createElement('h3');
+  h3.classList.add('name');
+  h3.textContent = data.name
+  // p username
+  let p = document.createElement('p');
+  p.classList.add('username');
+  p.textContent = data.login;
+  // p location
+  let location = document.createElement('p');
+  location.textContent = `Location: ${data.location}`;
+  let profile = document.createElement('p');
+  // profile link
+  let link = document.createElement('a');
+  link.setAttribute( 'href', data.html_url);
+  link.textContent = data.html_url;
+  // link within profile
+  profile.textContent = ` Profile: ${link}`;
+  profile.appendChild(link);
+  // p follower
+  let followers = document.createElement('p');
+  followers.textContent = `Followers: ${data.followers}`;
+  // p following
+  let following = document.createElement('p');
+  following.textContent = `Followers: ${data.following}`;
+  //  p bio
+  let bio = document.createElement('p');
+  bio.textContent = data.bio;
+// append child elements
+  card.appendChild(img);
+  card.appendChild(cardInfo);
+  cardInfo.appendChild(p);
+  cardInfo.appendChild(location);
+  cardInfo.appendChild(profile);
+  cardInfo.appendChild(followers);
+  cardInfo.appendChild(following);
+  cardInfo.appendChild(bio);
+
+  return card
+} 
+console.log(gitCardMaker(axios))
+
 
 /*
   STEP 4: Pass the data received from Github into your function,
